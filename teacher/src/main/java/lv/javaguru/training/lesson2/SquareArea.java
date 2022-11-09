b36aa0b9e15cab57000576ec03773e1da903bc6d
Olegs Ganzins <olegs.ganzins@atea.com>
2022-11-09 21:51:02 +0200

    Adding lesson 2 code.

---
+++ teacher/src/main/java/lv/javaguru/training/lesson2/SquareArea.java
@@ -0,0 +1,11 @@
+package lv.javaguru.training.lesson2;
+
+public class SquareArea {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 4;
+        System.out.println(a * b);
+    }
+
+}
