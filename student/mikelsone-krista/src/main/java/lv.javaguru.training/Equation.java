f98fdeae4ae3b995c9457a5df3481defc19831d4
KristaMikelsone <krista.krista123456@inbox.lv>
2022-11-16 20:07:48 +0200

    Adding Hello World

---
+++ student/mikelsone-krista/src/main/java/lv.javaguru.training/Equation.java
@@ -0,0 +1,13 @@
+package lv.javaguru.training;
+
+public class Equation {
+    public static void main(String[]args){
+        int x = 20;
+        int y = 7;
+        int z = 5;
+
+        int result = x + y * z;
+
+        System.out.println(result);
+    }
+}
