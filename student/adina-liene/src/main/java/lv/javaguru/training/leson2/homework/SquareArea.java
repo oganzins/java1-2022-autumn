bcbe7ec672159788803d518146fd8589baa8ad58
adina-liene <liene.adina@inbox.lv>
2022-11-16 20:25:53 +0200

    Adding homework

---
+++ student/adina-liene/src/main/java/lv/javaguru/training/leson2/homework/SquareArea.java
@@ -0,0 +1,20 @@
+package lv.javaguru.training.leson2.homework;
+
+public class SquareArea {
+
+    public static void main(String[] args) {
+        int side1 = 5;
+        int side2 = 3;
+        int side3 = 7;
+        int side4 = 8;
+
+        int area = side1 + side2 + side3 + side4;
+
+        System.out.println("Četrstūra perimetrs ir " + area + " metri!");
+
+
+    }
+
+
+
+}
