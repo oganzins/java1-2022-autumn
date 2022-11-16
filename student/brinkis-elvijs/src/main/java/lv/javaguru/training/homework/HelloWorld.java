78f4f4dd65034868de705a2f5b4c1ae27701db26
elvijs <elvijs3112@inbox.lv>
2022-11-16 19:45:10 +0200

    Adding HelloWorld

---
+++ student/brinkis-elvijs/src/main/java/lv/javaguru/training/homework/HelloWorld.java
@@ -0,0 +1,15 @@
+package lv.javaguru.training.homework;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        int a = 132;
+        int b = 231;
+        int sum = a+b;
+
+       System.out.println(sum);
+
+    }
+}
