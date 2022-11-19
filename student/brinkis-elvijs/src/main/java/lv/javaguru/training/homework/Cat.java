2fc59088f99358345e1bd7ffdf0cca84e2c24c2a
elvijs <elvijs3112@inbox.lv>
2022-11-19 23:01:43 +0200

    Adding HelloWorld

---
+++ student/brinkis-elvijs/src/main/java/lv/javaguru/training/homework/Cat.java
@@ -0,0 +1,54 @@
+package lv.javaguru.training.homework;
+
+public class Cat {
+
+    private String name;
+    private String color;
+    private String species;
+    private int age;
+    private int weight;
+
+    public Cat(String Roko, String brown, int age) {
+    }
+
+    String getName() {
+        return name;
+    }
+
+    String getSpecies() {
+        return species;
+    }
+
+    String getColor() {
+        return color;
+    }
+
+    int getAge() {
+        return age;
+    }
+
+    int getWeight() {
+        return weight;
+    }
+
+    public void setName(String Name) {
+        this.name = name;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public void setSpecies(String species) {
+    this.species = species;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+}
+
+
+
+
