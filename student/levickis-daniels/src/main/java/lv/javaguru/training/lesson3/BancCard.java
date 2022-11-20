e6036b7cba1af7c191eff03137102c9f9dd82af8
Danuxdrum <danuxdrum@inbox.lv>
2022-11-21 01:25:27 +0200

    Adding hallo world

---
+++ student/levickis-daniels/src/main/java/lv/javaguru/training/lesson3/BancCard.java
@@ -0,0 +1,20 @@
+package lv.javaguru.training.lesson3;
+
+public class BancCard {
+    String name;
+    String number;
+    String securityCode;
+
+    void printBalance(){
+
+    }
+
+    public static void main(String[] args) {
+        BancCard card = new BancCard();
+
+
+
+    }
+
+
+}
