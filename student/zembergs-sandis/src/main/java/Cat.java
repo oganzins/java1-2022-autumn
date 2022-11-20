be5f1ac2f6cdf2d0e77048c7e75172813d226cc7
Sandis Zembergs <szembergs@inbox.lv>
2022-11-20 20:25:24 +0000

    Cat

---
+++ student/zembergs-sandis/src/main/java/Cat.java
@@ -0,0 +1,13 @@
+public class Cat {
+
+    private String name;
+
+    public String getName() { return name;}
+
+    public void setNAme(String name) {this.name = name;}
+
+    public void voice() { System.out.println("Murr");}
+
+
+
+}
