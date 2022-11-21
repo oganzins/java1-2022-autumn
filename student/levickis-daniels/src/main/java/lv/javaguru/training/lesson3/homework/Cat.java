4c0d996570acce612ac46fae90b201bd242cd456
Danuxdrum <danuxdrum@inbox.lv>
2022-11-22 01:23:13 +0200

    Adding hallo world

---
+++ student/levickis-daniels/src/main/java/lv/javaguru/training/lesson3/homework/Cat.java
@@ -0,0 +1,54 @@
+package lv.javaguru.training.lesson3.homework;
+
+public class Cat {
+
+    public String name;
+    private String color;
+    private String species;
+    int age;
+    int weight;
+
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+public Cat () {
+}
+public Cat (String name, String color, int age){
+        this.name = name;
+        this.color = color;
+        this.age = age;
+}
+
+
+public String getName(){
+        return this.name;
+}
+public String getColor(){
+        return this.color;
+    }
+    public int getAge() {
+        return this.age;
+    }
+
+public void setName (String name){
+    this.name = name;
+
+        }
+public void setColor (String color){
+        this.color = color;
+}
+public void setAge (int age){
+        this.age = age;
+
+}
+
+    public void printInfo() {
+        System.out.println("Cat's name: " + name +", " + "Cat`s color" +": " + color + ", " + "Cat`s age:" + " " + age);
+    }
+
+
+
+
+}
---
+++ student/levickis-daniels/src/main/java/lv/javaguru/training/lesson3/homework/CatDemo.java
@@ -0,0 +1,29 @@
+package lv.javaguru.training.lesson3.homework;
+
+public class CatDemo {
+
+    public static void main(String[] args) {
+        Cat catWithOutName = new Cat();
+        Cat catName = new Cat("kakis");
+        Cat catAll =  new Cat("kakis","black", 9);
+        System.out.println(catAll.getName() + " " + catAll.getColor() + " " + catAll.getAge());
+        catAll.setName ("Lesters");
+        catAll.setColor ("Grey");
+        catAll.setAge (5);
+        System.out.println(catAll.getName() + " " + catAll.getColor() + " " + catAll.getAge());
+        catAll.printInfo();
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
