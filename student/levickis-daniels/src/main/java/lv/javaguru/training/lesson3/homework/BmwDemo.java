bf0086ff515c0b2fbf0641658c5078f1cc3fc6d9
Danuxdrum <danuxdrum@inbox.lv>
2022-11-22 01:36:08 +0200

    Adding hallo world

---
+++ student/levickis-daniels/src/main/java/lv/javaguru/training/lesson3/homework/BmwDemo.java
@@ -0,0 +1,14 @@
+package lv.javaguru.training.lesson3.homework;
+
+public class BmwDemo {
+    public static void main(String[] args) {
+        Bmw BmwDemo = new Bmw ("black",200);
+        System.out.println(BmwDemo.getColour() + BmwDemo.getPower());
+
+
+
+    }
+
+
+
+}
