84f5b36dab03c7b878e026352e33fc84c5cc16e9
renatevulfa <renate.vulfa@gmail.com>
2022-11-21 14:26:54 +0200

    Added HelloWorld

---
+++ student/vulfa-renate/src/main/java/lv/javaguru/training/lesson2/homework/HelloWorld.java
@@ -0,0 +1,10 @@
+package lv.javaguru.training.lesson2.homework;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+
+
+}
