26cd728c5e8674a56803ed68b4276511183883db
Olegs Ganzins <olegs.ganzins@atea.com>
2022-11-23 21:58:48 +0200

    Lesson 4

--- student/judins-deniss/src/main/java/lv/javaguru/training/lesson3/Homework/Cat.java
+++ student/judins-deniss/src/main/java/lv/javaguru/training/lesson3/Homework/Cat.java
@@ -6,51 +6,67 @@ public class Cat {
     String species;
     int age;
     float weight;
+
     Cat() {
     }
 
-    Cat(String name){}
+    Cat(String name) {
+
+    }
 
     Cat(String name, String color, int age) {
         this.name = name;
         this.color = color;
         this.age = age;
     }
-public String getCatName() {return name;}
-    public String getCatColor() {return color;}
-    public String getCatSpecies() {return species;}
-    public int getCatAge() {return age;}
-    public float getWeight() {return weight;}
+
+    public String getCatName() {
+        return name;
+    }
+
+    public String getCatColor() {
+        return color;
+    }
+
+    public String getCatSpecies() {
+        return species;
+    }
+
+    public int getCatAge() {
+        return age;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
 
 
-    public void setCatName(String newValue) {name = newValue;}
-    public void setCatColor(String newValue) {color = newValue;}
-    public void setCatSpecies(String newValue) {species = newValue;}
-    public void setCatAge(int newValue) {age = newValue;}
-    public void setCatWeight(float newValue) {weight = newValue;}
-}
+    public void setCatName(String newValue) {
+        name = newValue;
+    }
 
-class CatDemo {
-    private String catName;
-    private String catColor;
+    public void setCatColor(String newValue) {
+        color = newValue;
+    }
 
-    private int catAge;
+    public void setCatSpecies(String newValue) {
+        species = newValue;
+    }
 
-    public void main(String[] args) {
-        Cat cat = new Cat();
-        this.catName = "Garfield";
-        this.catColor = "Ginger";
-        this.catAge = 6;
+    public void setCatAge(int newValue) {
+        age = newValue;
+    }
 
-    Cat() {
-        }
+    public void setCatWeight(float newValue) {
+        weight = newValue;
+    }
 
-        Cat(this.catName){}
-
-        Cat(this.catName, this.catColor, this.catAge) {}
-
-        public void printInfo() {
-            System.out.println("Cat's name: " + this.catName);
-        }
+    public void printInfo() {
+        System.out.println("Cat's name: " + name);
+        System.out.println("Cat's age: " + age);
+        System.out.println("Cat's weight: " + weight);
+        System.out.println("Cat's color: " + color);
+        System.out.println("Cat's species: " + species);
     }
 }
+
---
+++ teacher/src/main/java/lv/javaguru/training/lesson4/Book.java
@@ -0,0 +1,37 @@
+package lv.javaguru.training.lesson4;
+
+public class Book {
+
+    private int pageCount;
+
+    public Book(int pageCount) {
+        this.pageCount = pageCount;
+    }
+
+    public String getSize() {
+        if (pageCount <= 50) {
+            return "Short";
+        } else if (pageCount > 50 && pageCount <= 100) {
+            return "Medium sized";
+        } else if (pageCount > 100 && pageCount <= 200) {
+            return "Long";
+        } else {
+            return "Very long";
+        }
+    }
+
+    public static void main(String[] args) {
+        Book shortBook = new Book(30);
+        System.out.println(shortBook.getSize());
+
+        Book mediumBook = new Book(80);
+        System.out.println(mediumBook.getSize());
+
+        Book longBook = new Book(150);
+        System.out.println(longBook.getSize());
+
+        Book veryLongBook = new Book(500);
+        System.out.println(veryLongBook.getSize());
+    }
+
+}
