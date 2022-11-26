7d31499dc59b0b55ef02d73230bebec8fa5bf2a5
judin <judins.deniss@gmail.com>
2022-11-26 16:19:38 +0200

    Mājasdarbi

--- student/judins-deniss/src/main/java/lv/javaguru/training/lesson3/Homework/CatDemo.java
+++ student/judins-deniss/src/main/java/lv/javaguru/training/lesson3/Homework/CatDemo.java
@@ -11,6 +11,20 @@ class CatDemo {
         cat.setCatName("Garfield");
         cat.setCatColor("Ginger");
         cat.setCatAge(6);
+        cat.setCatSpecies("None");
+        cat.setCatWeight(15);
+
+        Cat cat2 = new Cat();
+        cat2.setCatName("Garfield");
+
+        Cat cat3 = new Cat();
+        cat3.setCatName("Garfield");
+        cat3.setCatColor("Ginger");
+        cat3.setCatAge(6);
+
+        cat.printInfo();
+        cat2.printInfo();
+        cat3.printInfo();
 
 
     }
