cb1790d3a4aa3a5eb753e772482a552f638bcdb0
adina-liene <liene.adina@inbox.lv>
2022-11-29 14:58:11 +0200

    Adding homework

---
+++ student/adina-liene/src/main/java/lv/javaguru/training/lesson3/homework/PerfumeDemo.java
@@ -0,0 +1,20 @@
+package lv.javaguru.training.lesson3.homework;
+
+public class PerfumeDemo {
+
+
+    public static void main(String[] args) {
+
+        Perfume perfume;
+        perfume = new Perfume();
+        perfume.setName("Costa Azzura");
+        perfume.setBrand("Tom Ford");
+        perfume.setCountry("Sitzerland");
+        perfume.setPrice(180);
+        perfume.printInfo();
+
+
+    }
+
+
+}
