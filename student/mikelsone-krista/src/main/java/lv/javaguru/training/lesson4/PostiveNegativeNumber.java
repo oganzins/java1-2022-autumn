1932d4dd2232689bba6a419f32a6a663f43451e1
KristaMikelsone <krista.krista123456@inbox.lv>
2022-11-30 21:58:33 +0200

    Adding Homework

---
+++ student/mikelsone-krista/src/main/java/lv/javaguru/training/lesson4/PostiveNegativeNumber.java
@@ -0,0 +1,38 @@
+package lv.javaguru.training.lesson4;
+
+public class PostiveNegativeNumber {
+
+    public boolean isPositive(int number){
+
+        if (number >= 0){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isNegative(int number){
+
+        if (number <= 0){
+            return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        PostiveNegativeNumber positiveNegativeNumber = new PostiveNegativeNumber();
+
+        System.out.println(positiveNegativeNumber.isPositive(-1));
+        System.out.println(positiveNegativeNumber.isPositive(-5));
+        System.out.println(positiveNegativeNumber.isPositive(0));
+        System.out.println(positiveNegativeNumber.isPositive(3));
+        System.out.println(positiveNegativeNumber.isPositive(13));
+        System.out.println();
+        System.out.println(positiveNegativeNumber.isNegative(-1));
+        System.out.println(positiveNegativeNumber.isNegative(-5));
+        System.out.println(positiveNegativeNumber.isNegative(0));
+        System.out.println(positiveNegativeNumber.isNegative(3));
+        System.out.println(positiveNegativeNumber.isNegative(13));
+    }
+}
+
+
+
---
+++ student/mikelsone-krista/src/main/java/lv/javaguru/training/lesson5/WhileHellloWorld.java
@@ -0,0 +1,11 @@
+package lv.javaguru.training.lesson5;
+
+public class WhileHellloWorld {
+    public static void main(String[] args) {
+        int i = 0;
+        while(i<5){
+            System.out.println("HelloWorld");
+            i ++;
+        }
+    }
+}
