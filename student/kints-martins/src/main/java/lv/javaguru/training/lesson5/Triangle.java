cfb2f04e16049de37be08e9da20594f48e988693
MKints <mckints@inbox.lv>
2022-12-01 20:34:17 +0200

    adding homework 3

---
+++ student/kints-martins/src/main/java/lv/javaguru/training/lesson5/Triangle.java
@@ -0,0 +1,17 @@
+package lv.javaguru.training.lesson5;
+
+public class Triangle {
+
+    public static void main(String[] args) {
+        for (int i = 0; i< 9; i++) {
+            for (int j = 9 - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
