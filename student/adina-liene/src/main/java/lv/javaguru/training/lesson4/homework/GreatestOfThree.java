6ae7af3adbb3fb7884985acf6e32877e7038af84
adina-liene <liene.adina@inbox.lv>
2022-12-02 13:57:22 +0200

    Adding homework

---
+++ student/adina-liene/src/main/java/lv/javaguru/training/lesson4/homework/GreatestOfThree.java
@@ -0,0 +1,31 @@
+package lv.javaguru.training.lesson4.homework;
+
+public class GreatestOfThree {
+
+    public int getGreatest(int first, int second, int third) {
+
+        if (first > second && first > third) {
+            return first;
+        } else if (second > first && second > third) {
+            return second;
+        } else if (third > second && third > first) {
+            return third;
+        } else {
+            return first;
+
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        GreatestOfThree greatestOfThree = new GreatestOfThree();
+
+        System.out.println(greatestOfThree.getGreatest(1, 2, 3));
+        System.out.println(greatestOfThree.getGreatest(3, 4, 2));
+        System.out.println(greatestOfThree.getGreatest(3, 2, 1));
+        System.out.println(greatestOfThree.getGreatest(3, 3, 3));
+    }
+
+
+}
