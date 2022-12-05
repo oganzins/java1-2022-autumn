3d9e1cf398accd9cd1f3a4c267880f1e4d875fea
Juris Dambenieks <jurelis1991@gmail.com>
2022-12-05 19:03:48 +0100

    5. nodarbības mājas darbs!

---
+++ student/dambenieks-juris/src/main/java/Lesson5/homework/DivideByThirteen.java
@@ -0,0 +1,17 @@
+package Lesson5.homework;
+
+public class DivideByThirteen {
+
+    public static void main(String[] args) {
+        int i = 0;
+
+        while (i <= 100) {
+            if(i % 13 == 0)
+            {
+                System.out.println(i);
+            }
+            i++;
+
+        }
+    }
+}
---
+++ student/dambenieks-juris/src/main/java/Lesson5/homework/WorkOut.java
@@ -0,0 +1,15 @@
+package Lesson5.homework;
+
+public class WorkOut {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 15; i++) {
+            if (i < 10) {
+                System.out.println("Push-up");
+            } else
+                System.out.println("Star jump");
+            }
+        }
+    }
+
