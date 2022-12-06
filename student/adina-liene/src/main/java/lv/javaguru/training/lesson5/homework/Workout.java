37b77a634ba3024a10769051d3b17bb96b6bdbd6
adina-liene <liene.adina@inbox.lv>
2022-12-06 10:58:36 +0200

    Adding homework

---
+++ student/adina-liene/src/main/java/lv/javaguru/training/lesson5/homework/Workout.java
@@ -0,0 +1,20 @@
+package lv.javaguru.training.lesson5.homework;
+
+public class Workout {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 15; i++) {
+            if (i < 10) {
+                System.out.println("Push-up");
+            } else System.out.println("Star jump");
+
+        }
+
+
+    }
+
+
+}
+
+
