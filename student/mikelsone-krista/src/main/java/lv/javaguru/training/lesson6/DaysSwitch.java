5ffdb292fe1b67183a027d4bbf9359968bcd96e7
KristaMikelsone <krista.krista123456@inbox.lv>
2022-12-07 22:16:00 +0200

    Adding Homework

---
+++ student/mikelsone-krista/src/main/java/lv/javaguru/training/lesson6/DaysSwitch.java
@@ -0,0 +1,42 @@
+package lv.javaguru.training.lesson6;
+
+public class DaysSwitch {
+    public static void main(String[] args) {
+        System.out.println(getDay(5));
+    }
+    public static String getDay(int dayNumber) {
+        String day;
+        switch(dayNumber) {
+            case 1: {
+                day = "Monday";
+                break;
+            }
+            case 2: {
+                day = "Tuesday";
+                break;
+            }
+            case 3: {
+                day = "Wednesday";
+                break;
+            }
+            case 4: {
+                day = "Thursday";
+                break;
+            }
+            case 5: {
+                day = "Friday";
+                break;
+            }
+            case 6: {
+                day = "Saturday";
+                break;
+            }
+            case 7: {
+                day = "Sunday";
+                break;
+            }
+        }
+        return "";
+    }
+
+}
