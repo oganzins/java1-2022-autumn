4a2f44ce7029b0f9803fe18719468688a794e23a
elvijs <elvijs3112@inbox.lv>
2022-12-07 16:04:45 +0200

    Adding HelloWorld

--- student/brinkis-elvijs/src/main/java/lv/javaguru/training/homework/homework5/DivideByThirteen.java
+++ student/brinkis-elvijs/src/main/java/lv/javaguru/training/homework/homework5/DivideByThirteen.java
@@ -2,6 +2,12 @@ package lv.javaguru.training.homework.homework5;
 
 public class DivideByThirteen {
     public static void main(String[] args) {
-
+ int i = 0;
+ while (i <= 100) {
+     if(i % 13 ==0) {
+         System.out.println(i);
+     }
+     i++;
+ }
     }
 }
