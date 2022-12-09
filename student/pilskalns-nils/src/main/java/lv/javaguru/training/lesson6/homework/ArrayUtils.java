0421fa85d5f7c56513c889c5c9594ac1ccda837f
Olegs Ganzins <olegs.ganzins@atea.com>
2022-12-09 21:37:29 +0200

    Lesson 6 Comments for student homework

--- student/pilskalns-nils/src/main/java/lv/javaguru/training/lesson6/homework/ArrayUtils.java
+++ student/pilskalns-nils/src/main/java/lv/javaguru/training/lesson6/homework/ArrayUtils.java
@@ -5,6 +5,25 @@ import java.util.Arrays;
 public class ArrayUtils {
     public int[] copy(int[] sourceArray) {
         for (int i = 0; i < sourceArray.length; i++) {
+            /*
+                Šeit nekas netiek kopēts, bet gan tiek izvadīts konsolē.
+                System.out.println - izvada nevis kopē.
+
+                Šai funkcijai ir jātgriež jauns masīvs, kas ir kopija sourceArray massīvam
+
+                1. Ir jāizveido jauns masīvs
+                    int[] copyArray = new int[sourceArray.length];
+                2. copyArray ir jāsarasta sourceArray dati
+                    for (int i = 0; i < sourceArray.length; i++) {
+                        copyArray[i] = sourceArray[i];
+                    }
+                3. Metodes beigās ir jāatgriež copyArray
+                   return copyArray
+
+                   Līdzīgi ir jāizdara arī reverse metodē. Arī tur pašlaik tiek izvadīts masīvs
+                   nevis izveidots jauns masīvs, kas satur apgriestu sourceArray.
+
+             */
             System.out.print(sourceArray [i] + " ");
         }
 
