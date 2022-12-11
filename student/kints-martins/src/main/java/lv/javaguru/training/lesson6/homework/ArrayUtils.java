1fd64378e8e09f013e07496fb9e46fb16c722db4
MKints <mckints@inbox.lv>
2022-12-11 15:13:21 +0200

    adding homework 3

---
+++ student/kints-martins/src/main/java/lv/javaguru/training/lesson6/homework/ArrayUtils.java
@@ -0,0 +1,16 @@
+package lv.javaguru.training.lesson6.homework;
+
+public class ArrayUtils {
+
+    public int[] copy(int[] sourceArray) {
+
+        return sourceArray;
+
+    }
+
+    int[] reverse(int[] sourceArray){
+
+        return sourceArray;
+    }
+
+}
---
+++ student/kints-martins/src/main/java/lv/javaguru/training/lesson6/homework/PrintMyName.java
@@ -0,0 +1,15 @@
+package lv.javaguru.training.lesson6.homework;
+
+public class PrintMyName {
+
+    public static void main(String[] args) {
+
+        char[] myName = {'M', 'Ā', 'R', 'T', 'I', 'Ņ', 'Š'};
+
+        for (int i = 0; i <7; i++) {
+            System.out.print(myName[i]);
+        }
+
+    }
+
+}
