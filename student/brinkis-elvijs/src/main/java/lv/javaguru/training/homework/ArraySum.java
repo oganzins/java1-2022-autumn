36e1adc523c82a65c4f2ac1f6cf271ed39fce7d6
elvijs <elvijs3112@inbox.lv>
2022-12-12 15:18:11 +0200

    Adding HelloWorld

---
+++ student/brinkis-elvijs/src/main/java/lv/javaguru/training/homework/ArraySum.java
@@ -0,0 +1,16 @@
+package lv.javaguru.training.homework;
+
+import lv.javaguru.training.lesson4.QuickMath;
+
+public class ArraySum {
+    public static void main(String[] args) {
+
+        int[] values = {1, 3, 9, 11, 20};
+
+
+
+
+
+
+    }
+}
