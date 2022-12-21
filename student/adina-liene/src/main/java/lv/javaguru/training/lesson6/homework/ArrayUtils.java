add7f2b044f6f24e4cb83fee07c2a6ada5dffbba
adina-liene <liene.adina@inbox.lv>
2022-12-21 14:26:04 +0200

    Adding homework

---
+++ student/adina-liene/src/main/java/lv/javaguru/training/lesson6/homework/ArrayUtils.java
@@ -0,0 +1,21 @@
+package lv.javaguru.training.lesson6.homework;
+
+public class ArrayUtils {
+
+    public int[] copy(int[] sourceArray) {
+        int[] copy = new int[sourceArray.length];
+        for (int i = 0; i < sourceArray.length; i++) {
+            copy[i] = sourceArray[i];
+        }
+        return copy;
+    }
+
+    public int[] reverse(int[] sourceArray) {
+        int[] reverseArray = new int[sourceArray.length];
+        for (int i = 0; i < sourceArray.length; i++) {
+            reverseArray[sourceArray.length - 1 - i] = sourceArray[i];
+        }
+        return reverseArray;
+    }
+
+}
