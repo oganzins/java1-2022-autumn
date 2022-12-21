4abdd5449e83d344d8dd57e4abe7697921ed44f6
Danuxdrum <danuxdrum@inbox.lv>
2022-12-21 18:41:52 +0200

    Adding hallo world

---
+++ student/levickis-daniels/src/main/java/lv/javaguru/training/lesson7/CreditCard.java
@@ -0,0 +1,85 @@
+package lv.javaguru.training.lesson7;
+
+public class CreditCard {
+
+    private String name;
+    private String number;
+    private double balance;
+    private String pinCode;
+    private int invalidPinCodeCounter;
+    boolean active;
+    boolean authorized;
+
+
+    public CreditCard(String name, String number, String pinCode, double balance) {
+        this.name = name;
+        this.number = number;
+        this.pinCode = pinCode;
+        this.balance = balance;
+    }
+
+    public CreditCard(String name, String number, String pinCode) {
+        this(name, number, pinCode, 0.00);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public boolean isAuthorized() {
+        return active;
+    }
+
+    public void login(String pinCode) {
+        if (isActive() == true && pinCode == this.pinCode) {
+
+            authorized = true;
+        } else authorized = false;
+
+        if (pinCode == this.pinCode) {
+            invalidPinCodeCounter = 0;
+
+
+        } else invalidPinCodeCounter += 0;
+        if (invalidPinCodeCounter == 3) {
+            active = false;
+
+        }
+    }
+
+
+    public void logout() {
+        authorized = false;
+
+
+    }
+
+    public void deposit(double amount) {
+        if (authorized == true) {
+            amount = amount + balance;
+
+        } else amount = amount;
+
+    }
+
+    public void withdraw(double amount) {
+        if (authorized == true && balance > amount) {
+            balance = balance - amount;
+
+        }
+
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+}
