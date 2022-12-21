e03f94c6d081d05b81dfcf299f51d6caf467f4ff
KristaMikelsone <krista.krista123456@inbox.lv>
2022-12-21 10:28:28 +0200

    Adding Homework

--- student/mikelsone-krista/src/main/java/lv/javaguru/training/lesson6/ArraySum.java
+++ student/mikelsone-krista/src/main/java/lv/javaguru/training/lesson6/ArraySum.java
@@ -3,11 +3,9 @@ package lv.javaguru.training.lesson6;
 public class ArraySum {
     public static void main(String[] args) {
         int[] numbers = {1, 3, 9, 11, 20};
-        numbers[4] = 5;
         int sum = 0;
-        for (int i = 0; i < numbers.length; i++){
+        for (int i = 0; i < numbers.length; i++) {
             sum += numbers[i];
-            i++;
         }
         System.out.println(sum);
     }
---
+++ student/mikelsone-krista/src/main/java/lv/javaguru/training/lesson7/Rectangle.java
@@ -0,0 +1,33 @@
+package lv.javaguru.training.lesson7;
+
+public class Rectangle {
+
+    private int width;
+    private int height;
+
+    public Rectangle(int width, int height){
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int calculateArea(){
+        int area;
+        area = width * height;
+        return area;
+    }
+
+    public int calculatePerimeter(){
+        int perimeter;
+        perimeter = (width + height) * 2;
+        return perimeter;
+    }
+}
