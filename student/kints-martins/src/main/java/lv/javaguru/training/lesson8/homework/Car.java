624e5ec38acfd3f511337b32ecac9a90365b1899
MKints <mckints@inbox.lv>
2023-01-01 19:33:58 +0200

    adding homework 3

---
+++ student/kints-martins/src/main/java/lv/javaguru/training/lesson8/homework/Car.java
@@ -0,0 +1,50 @@
+package lv.javaguru.training.lesson8.homework;
+
+import java.util.Objects;
+
+public class Car {
+
+    protected String color;
+    protected int year;
+    protected String make;
+    protected String model;
+
+    public Car(String color, int year, String make, String model) {
+        this.color = color;
+        this.year = year;
+        this.make = make;
+        this.model = model;
+    }
+
+    public String getColor() {return color;
+    }
+    public int getYear() {return year;
+    }
+    public String getMake() {return make;
+    }
+    public String getModel() {return model;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "color='" + color + '\'' +
+                ", year=" + year +
+                ", make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return year == car.year && Objects.equals(color, car.color) && Objects.equals(make, car.make) && Objects.equals(model, car.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(color, year, make, model);
+    }
+}
---
+++ student/kints-martins/src/main/java/lv/javaguru/training/lesson8/homework/Truck.java
@@ -0,0 +1,41 @@
+package lv.javaguru.training.lesson8.homework;
+
+import java.util.Objects;
+
+public class Truck extends Car {
+
+    protected int loadCapacity;
+
+    public Truck(String color, int year, String make, String model, int loadCapacity) {
+        super(color, year, make, model);
+        this.loadCapacity = loadCapacity;
+    }
+
+    public int getLoadCapacity() {return loadCapacity;
+    }
+
+    @Override
+    public String toString() {
+        return "Truck{" +
+                "loadCapacity=" + loadCapacity +
+                ", color='" + color + '\'' +
+                ", year=" + year +
+                ", make='" + make + '\'' +
+                ", model='" + model + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Truck truck = (Truck) o;
+        return loadCapacity == truck.loadCapacity;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), loadCapacity);
+    }
+}
