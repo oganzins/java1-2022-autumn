7f403ea5b219f057c08df3e578e294139a254827
Nils Pilskalns <nilspilskalns@inbox.lv>
2023-01-19 19:08:10 +0200

    Adding homework 11

---
+++ student/pilskalns-nils/src/main/java/lv/javaguru/training/lesson11/homework/SimpleInventory.java
@@ -0,0 +1,112 @@
+package lv.javaguru.training.lesson11.homework;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SimpleInventory implements Inventory {
+
+    private Map<String, Product> products;
+
+    public SimpleInventory() {
+        products = new HashMap<>();
+    }
+
+    @Override
+    public void addProduct(Product product) {
+        if (isPresent(product)) {
+            products.put(product.getName(), product);
+        }
+    }
+
+    @Override
+    public Product getProduct(String productName) {
+        return products.get(productName);
+    }
+
+    @Override
+    public int getProductCount() {
+        return products.size();
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return (List<Product>) products.values();
+    }
+
+    @Override
+    public int getProductQuantity(String productName) {
+        int productQuantity = 0;
+        for (Product product : products.values()) {
+            if (product.getName().equals(productName)) {
+                productQuantity = product.getQuantity();
+            }
+        }
+        return productQuantity;
+    }
+
+    @Override
+    public void updateProductQuantity(String productName, int newQuantity) {
+        for (Product product : products.values()) {
+            if (product.getName().equals(productName)) {
+                product.setQuantity(newQuantity);
+            }
+        }
+    }
+
+    @Override
+    public double getProductPrice(String productName) {
+        double productPrice = 0;
+        for (Product product : products.values()) {
+            if (product.getName().equals(productName)) {
+                productPrice = product.getPrice();
+            }
+        }
+        return productPrice;
+    }
+
+    @Override
+    public void updateProductPrice(String productName, double newPrice) {
+        for (Product product : products.values()) {
+            if (product.getName().equals(productName)) {
+                product.setPrice(newPrice);
+            }
+        }
+
+    }
+
+    @Override
+    public Product getProductWithLowestPrice() {
+        Product cheapestProduct = null;
+        for (Product product : products.values()) {
+            if (cheapestProduct == null || cheapestProduct.getPrice() > product.getPrice()) {
+                cheapestProduct = product;
+            }
+        }
+
+        return cheapestProduct;
+    }
+
+    @Override
+    public Product getProductWithHighestPrice() {
+
+        Product expensiveProduct = null;
+        for (Product product : products.values()) {
+            if (expensiveProduct == null || expensiveProduct.getPrice() < product.getPrice()) {
+                expensiveProduct = product;
+            }
+        }
+
+        return expensiveProduct;
+    }
+
+    @Override
+    public void removeProduct(String productName) {
+        products.remove(productName);
+    }
+
+    private boolean isPresent(Product product) {
+        return !products.containsKey(product.getName());
+    }
+}
