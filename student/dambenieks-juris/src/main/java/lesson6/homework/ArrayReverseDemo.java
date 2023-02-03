6438d978e44abe64d0f33e075e960bc00ef85a9a
Olegs Ganzins <olegs.ganzins@gmail.com>
2023-02-03 09:13:58 +0200

    Fixing code to compile

--- student/dambenieks-juris/src/main/java/lesson6/homework/ArrayReverseDemo.java
+++ student/dambenieks-juris/src/main/java/lesson6/homework/ArrayReverseDemo.java
@@ -6,12 +6,12 @@ public class ArrayReverseDemo {
 
     public static void main(String[] args) {
         int[] source = {1, 3, 5, 7};
-        ArrayUtils arrayUtils = new ArrayUtils();
-        int[] reversedArray = arrayUtils.reverse(source);
+        //ArrayUtils arrayUtils = new ArrayUtils();
+        //int[] reversedArray = arrayUtils.reverse(source);
 
 
 
         System.out.println(Arrays.toString(source));
-        System.out.println(Arrays.toString(reversedArray));
+        //System.out.println(Arrays.toString(reversedArray));
     }
 }
