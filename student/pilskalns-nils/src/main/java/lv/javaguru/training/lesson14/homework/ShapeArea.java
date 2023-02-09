010dacf07d626f571a427af28277fa1f28b0743e
Nils Pilskalns <nilspilskalns@inbox.lv>
2023-02-09 14:43:52 +0200

    Adding homework 14

---
+++ student/pilskalns-nils/src/main/java/lv/javaguru/training/lesson14/homework/ShapeArea.java
@@ -0,0 +1,28 @@
+package lv.javaguru.training.lesson14.homework;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class ShapeArea {
+    public static Function<Double, Double> squareArea() {
+        Function<Double, Double> squareResult = (a) -> a * a;
+        return squareResult;
+    }
+
+    public static BiFunction<Double, Double, Double> rectangleArea() {
+        BiFunction<Double, Double, Double> rectangleResult = (a, b) -> a * b;
+        return rectangleResult;
+    }
+
+    public static Function<Double, Double> circleArea() {
+            Function<Double, Double> circleResult = (a) -> a * a * 3.14;
+
+        return circleResult;
+    }
+
+    public static BiFunction<Double, Double, Double> triangleArea() {
+        BiFunction<Double, Double, Double> triangleResult = (a, b) -> a * b / 2;
+        return triangleResult;
+    }
+
+}
