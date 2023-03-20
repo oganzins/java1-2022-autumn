10b6021a7b45367fbebb419ed152ae1650d7964a
renatevulfa <renate.vulfa@gmail.com>
2023-03-20 09:20:37 +0200

    Added homework 2

---
+++ student/vulfa-renate/src/main/java/lv/javaguru/training/lesson2/homework/Equation.java
@@ -0,0 +1,12 @@
+package lv.javaguru.training.lesson2.homework;
+
+public class Equation {
+    public static void main(String[] args){
+        int x = 2;
+        int y = 7;
+        int z = 4;
+        int result = x + y * z;
+
+        System.out.println(result);
+    }
+}
