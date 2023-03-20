6eac8a600d3af1860c7d3da30f8313286741609d
renatevulfa <renate.vulfa@gmail.com>
2023-03-20 09:32:45 +0200

    Added homework 3

---
+++ student/vulfa-renate/src/main/java/lv/javaguru/training/lesson3/homework/Book.java
@@ -0,0 +1,37 @@
+package lv.javaguru.training.lesson3.homework;
+
+public class Book {
+
+    String title;
+    String author;
+    int pageCount;
+
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setAuthor(String author){
+        this.author = author;
+    }
+
+    public void setPageCount(int pageCount){
+        this.pageCount = pageCount;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+
+}
